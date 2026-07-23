ea14f7f7cd79e79ff25ad0c6988c9d4741e5190e
eltrueno <truenodeveloper@gmail.com>
2026-07-23 15:30:41 +0200

    Update CaptainPanel.vue

--- apps/frontend/src/components/captain/CaptainPanel.vue
+++ apps/frontend/src/components/captain/CaptainPanel.vue
@@ -554,7 +554,7 @@ function formatMatchScore(teamData: any) {
         <p v-if="clubSearchError" class="text-error text-xs mb-2">{{ clubSearchError }}</p>
         <ul v-if="clubResults.length" class="menu bg-base-200 rounded-box mb-3 max-h-48 overflow-y-auto flex-nowrap">
           <li v-for="c in clubResults" :key="c.clubId">
-            <a @click="pickClub(c.clubId)" class="flex items-center gap-2">
+            <a @click="pickClub(c.clubId, c.name)" class="flex items-center gap-2">
               <img v-if="eaCrestUrl(c.teamId, c.crestAssetId, c.customKit)" :src="eaCrestUrl(c.teamId, c.crestAssetId, c.customKit)!" class="w-6 h-6 object-contain shrink-0" />
               <div v-else class="w-6 h-6 rounded bg-base-300 shrink-0"></div>
               <span class="flex-1">{{ c.name }}</span>
